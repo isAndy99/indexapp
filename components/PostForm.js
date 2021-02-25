90fa32e6b83dbb436d84c0d9c0f11be4744227ac
Andrei Taran <andrei@dotts.ro>
2021-02-25 02:17:33 +0200

    refactor postform

--- components/PostForm.js
+++ components/PostForm.js
@@ -1,25 +1,45 @@
 import React, { useState } from "react";
 
 export const PostForm = ({ postData, onCancel, onSave }) => {
-  const [title, setTitle] = useState(postData.title);
-  const [body, setBody] = useState(postData.body);
+  const [formData, setFormData] = useState({
+    title: postData.title,
+    body: postData.body,
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.currentTarget;
+    setFormData((prevState) => ({ ...prevState, [name]: value }));
+  };
 
   return (
     <>
+      <h2>Edit Post</h2>
       <label htmlFor="title">Title</label>
       <input
         id="title"
         name="title"
-        onChange={(e) => setTitle(e.target.value)}
-        value={title}
+        onChange={handleChange}
+        value={formData.title}
       />
+      <br />
+      <label htmlFor="body">Content</label>
       <textarea
+        id="body"
+        name="body"
         style={{ width: "100%", height: "70%", resize: "none" }}
-        onChange={(e) => setBody(e.target.value)}
-        value={body}
+        onChange={handleChange}
+        value={formData.body}
       />
       <button onClick={onCancel}>Cancel</button>
-      <button onClick={() => onSave({ id: postData.id, title, body })}>
+      <button
+        onClick={() =>
+          onSave({
+            id: postData.id,
+            title: formData.title,
+            body: formData.body,
+          })
+        }
+      >
         Save
       </button>
     </>
